8ec54dfe04fab20392c318a81e8851da9719dfcf
keremcengiz0 <keremcengiz0@hotmail.com>
2023-03-01 14:54:46 +0300

    Initial project add.

---
+++ src/test/java/com/keremcengiz0/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keremcengiz0.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
