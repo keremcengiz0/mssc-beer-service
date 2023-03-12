2735ec536e0b02c6a2931eaac32a39a1a095a8f3
keremcengiz0 <keremcengiz0@hotmail.com>
2023-03-12 13:32:21 +0300

    added ehcache for listBeers and getBeerById.

--- src/main/java/com/keremcengiz0/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/com/keremcengiz0/msscbeerservice/services/BeerServiceImpl.java
@@ -8,6 +8,7 @@ import com.keremcengiz0.msscbeerservice.web.model.BeerDto;
 import com.keremcengiz0.msscbeerservice.web.model.BeerPagedList;
 import com.keremcengiz0.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,11 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
+    @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
+
+        System.out.println("I was called.");
 
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
@@ -65,6 +70,7 @@ public class BeerServiceImpl implements BeerService {
         return beerPagedList;
     }
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerDto getBeerById(UUID beerId, Boolean showInventoryOnHand) {
         Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
